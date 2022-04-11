7227bbfc9a9f6a0bbd0de5d07142cfea10d2fc58
timbo-graham <tmgraham29@gmail.com>
2022-04-11 16:22:06 +0100

    Students array is now populated by user input

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "The Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit the return (enter) key twice"
+
+  # Create the empty array students, to be filled by the block below
+  students = []
+  name = gets.chomp
+
+  # While the user's input isn't blank, execute the block below
+  while !name.empty? do
+    # Use the shovel operator (<<) to chuck the input into the students array. Each student is a hash, and we assign name to the key name:. Cohort is hard-coded.
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+    name = gets.chomp
+  end
+  # Return the array of students, so it can be accessed by the rest of the code (i.e. stored in the variable students)
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +32,7 @@ def print_footer(students)
   print "Overall, we have #{students.count} great students."
 end
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)

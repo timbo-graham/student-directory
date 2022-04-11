9818f1d64dc4ed6e398da2af1833c800eeb97f53
timbo-graham <tmgraham29@gmail.com>
2022-04-11 13:10:05 +0100

    Added comments for clarity

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# First, we add the list of students at the Academy
 puts "The students of Villains Academy"
 puts "--------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Then, we print a headcount
 print "Overall, we have "
 print 11
+# It's important to use print() here, because we don't want new line characters
 print " great students."
